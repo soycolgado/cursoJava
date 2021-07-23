f920e7bc327a357aea99a873fc04cf988f79e4bf
nestor <soycolgado@gmail.com>
2021-07-23 11:50:52 -0300

    hash

---
+++ colecciones/src/org/nestorbardel/ejemplos/set/EjemploHashSetAgregar.java
@@ -0,0 +1,27 @@
+package org.nestorbardel.ejemplos.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EjemploHashSetAgregar {
+    public static void main(String[] args) {
+
+        Set<String> hs = new HashSet<>();
+        hs.add("uno");
+        hs.add("dos");
+        hs.add("tres");
+        hs.add("cuatro");
+        hs.add("cinco");
+
+        System.out.println(hs);
+
+        boolean b = hs.add("tres");
+
+        System.out.println(b);
+
+        System.out.println(hs);
+
+
+
+    }
+}
