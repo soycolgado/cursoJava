67f697fda2411fbc8af61c419076cb02b9b07a81
nestor <soycolgado@gmail.com>
2021-08-10 20:12:18 -0300

    Singleton

--- patrones_diseno/src/org/nestorbardel/patrones/singleton/ConexionBdSingleton.java
+++ patrones_diseno/src/org/nestorbardel/patrones/singleton/ConexionBdSingleton.java
@@ -1,4 +1,18 @@
 package org.nestorbardel.patrones.singleton;
 
 public class ConexionBdSingleton {
+
+    private static ConexionBdSingleton instancia;
+
+    private ConexionBdSingleton(){
+        System.out.println("Conectandose a algun motor de base de datos");
+    }
+
+    public static ConexionBdSingleton getInstancia(){
+        if(ConexionBdSingleton.instancia == null){
+            ConexionBdSingleton.instancia = new ConexionBdSingleton();
+        }
+
+        return ConexionBdSingleton.instancia;
+    }
 }
